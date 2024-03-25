ed45924002b75dea876af6a37736ba3a5cd3de9c
Yash Patel <155745623+yashpatel4521@users.noreply.github.com>
2024-03-25 14:33:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = [];
 
     const cardArray = [
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card1', img: 'images/distracted.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card2', img: 'images/drake.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card3', img: 'images/fine.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card4', img: 'images/rollsafe.png' },
-        { name: 'card5', img: 'images/success.png' },
-        { name: 'card5', img: 'images/success.png' },
+        { name: 'card1', img: 'image/distracted.png' },
+        { name: 'card1', img: 'image/distracted.png' },
+        { name: 'card2', img: 'image/drake.png' },
+        { name: 'card2', img: 'image/drake.png' },
+        { name: 'card3', img: 'image/fine.png' },
+        { name: 'card3', img: 'image/fine.png' },
+        { name: 'card4', img: 'image/rollsafe.png' },
+        { name: 'card4', img: 'image/rollsafe.png' },
+        { name: 'card5', img: 'image/success.png' },
+        { name: 'card5', img: 'image/success.png' },
         // ...add more pairs as needed
     ];
 
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         for (let i = 0; i < cardArray.length; i++) {
             const card = document.createElement('img');
-            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('src', 'image/blank.png');
             card.setAttribute('data-id', i);
             card.addEventListener('click', flipCard);
             grid.appendChild(card);
@@ -61,8 +61,8 @@ document.addEventListener('DOMContentLoaded', () => {
             cards[secondCardId].removeEventListener('click', flipCard);
             cardsWon.push(cardsChosen);
         } else {
-            cards[firstCardId].setAttribute('src', 'images/blank.png');
-            cards[secondCardId].setAttribute('src', 'images/blank.png');
+            cards[firstCardId].setAttribute('src', 'image/blank.png');
+            cards[secondCardId].setAttribute('src', 'image/blank.png');
         }
 
         cardsChosen = [];
